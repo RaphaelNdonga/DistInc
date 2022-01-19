20174f7c4477c7c194f68656005e14cfee138b0e
RaphaelNdonga <nkibi53@gmail.com>
2022-01-19 15:21:23 +0300

    The contract now can be deployed on any network

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,5 +1,13 @@
-from brownie import accounts
+from brownie import network, config, accounts
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
 
 def get_account():
-    return accounts[0]
+    if (network.show_active() in FORKED_LOCAL_ENVIRONMENTS) or (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
